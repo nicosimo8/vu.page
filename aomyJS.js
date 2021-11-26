c7f85a1c1b8980ffa69bdd4999adae509d901422
QouB <racingclub_nico@hotmail.com>
2021-11-25 22:56:21 -0300

    first commit

---
+++ aomyJS.js
@@ -0,0 +1,9 @@
+function ocultar() {
+    document.getElementById('welcome_text').style.display = 'none';
+    document.getElementById('support_text').style.display = 'block';
+}
+
+function mostrar() {
+    document.getElementById('welcome_text').style.display = 'block';
+    document.getElementById('support_text').style.display = 'none';
+}
